15ec35bd1011d30c51b4bc209cf86e1296b4158f
rizalyoga <ach.rizal.yogaswara@gmail.com>
2023-09-05 22:05:33 +0700

    webview quran-app

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import {
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+} from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
+  const [canGoBack, setCanGoBack] = useState(false);
+  const [canGoForward, setCanGoForward] = useState(false);
+  const [currentUrl, setCurrentUrl] = useState("");
+
+  const webviewRef = useRef(null);
+
+  backButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goBack();
+  };
+
+  frontButtonHandler = () => {
+    if (webviewRef.current) webviewRef.current.goForward();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar translucent={false} style="light" />
+
+      <WebView
+        source={{ uri: "https://quran-rizalyoga.vercel.app/" }}
+        originWhitelist={["*"]}
+        startInLoadingState={true}
+        renderLoading={() => (
+          <ActivityIndicator
+            color="blue"
+            size="large"
+            style={{
+              flex: 1,
+            }}
+          />
+        )}
+        ref={webviewRef}
+        onNavigationStateChange={(navState) => {
+          setCanGoBack(navState.canGoBack);
+          setCanGoForward(navState.canGoForward);
+          setCurrentUrl(navState.url);
+        }}
+      />
+      {/* <View
+        style={{
+          width: 400,
+          padding: 10,
+          flexDirection: "row",
+          justifyContent: "space-around",
+          backgroundColor: "#2ed2d2",
+        }}
+      >
+        <TouchableOpacity onPress={backButtonHandler}>
+          <Text style={{ color: "white", fontSize: 20 }}>Back</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={frontButtonHandler}>
+          <Text style={{ color: "white", fontSize: 20 }}>Forward</Text>
+        </TouchableOpacity>
+      </View> */}
     </View>
   );
 }
@@ -13,8 +71,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#252839",
+    // justifyContent: "center",
+  },
+  tittles: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  subText: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
